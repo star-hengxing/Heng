711acff1f07e2e8a360ea8faeb609990b05caefa
star-hengxing <hengxings783@gmail.com>
2021-10-30 16:25:33 +0800

    fix uszie iszie

--- include/type.h
+++ include/type.h
@@ -10,8 +10,8 @@ typedef unsigned short     u16;
 typedef unsigned int       u32;
 typedef unsigned long long u64;
 
-typedef u64 uszie;
-typedef i64 iszie;
+typedef u64 usize;
+typedef i64 isize;
 
 #define NULL  ((void*)0)
 #define bool  i32
