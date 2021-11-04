0d0657496d5197ea4071c13e141f3e13ea078931
star-hengxing <hengxings783@gmail.com>
2021-11-04 12:39:52 +0800

    update memset

--- src/string.c
+++ src/string.c
@@ -3,10 +3,14 @@
 
 void memset(void* dest, isize value, usize size)
 {
-    for(usize i = 0; i < size; i += sizeof(i8))
-    {
-        ((i8*)dest)[i] = value;
-    }
+    asm
+    (
+        // "mov %0, %rdi\n"
+        "mov %0, %%rax\n"
+        "mov %1, %%rcx\n"
+        "rep stosb\n"
+        :: "r"(value), "r"(size)
+    );
 }
 
 void memcpy(void* dest, void* src, usize size)
